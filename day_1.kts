36218e85a035b197ac5a869c741aed6fa6619c09
Pedro Costa <pedro.costa2@sky.uk>
2021-12-02 21:00:43 +0000

    day1 with reduce

--- day_1.kts
+++ day_1.kts
@@ -5,23 +5,12 @@ fun day1() {
         .readLines()
         .map { it.toInt() }
 
-    var currReading = input.first()
     var acc = 0
-
-    input.drop(0).forEach {
-        if (it > currReading) acc += 1
-        currReading = it
-    }
+    input.reduce { a, b -> if (b > a) acc++; b }
     println("sol1: $acc")
 
-    val input2 = input.windowed(3, 1).map { it.sum() }
-    currReading = input2.first()
     acc = 0
-
-    input2.drop(0).forEach {
-        if (it > currReading) acc += 1
-        currReading = it
-    }
+    input.windowed(3, 1).map { it.sum() }.reduce { a, b -> if (b > a) acc++; b }
     println("sol2: $acc")
 }
 
