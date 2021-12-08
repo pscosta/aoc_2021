f20010f9cb1553c76401ee8f11f4149fe28cf439
Pedro Costa <pedro.costa2@sky.uk>
2021-12-08 20:23:13 +0000

    day 8 golfing

--- day_8.kts
+++ day_8.kts
@@ -6,53 +6,44 @@ val displays = java.io.File("/in/input8.txt")
     .map { it.split(" | ") }
     .map { Display(it[0].split(" ").map { it.toSet() }, it[1].split(" ")) }
 
-fun day8() {
-    val sol2 = displays.sumOf { display ->
-        lateinit var `1`: Set<Char>
-        lateinit var `7`: Set<Char>
-        lateinit var `4`: Set<Char>
+fun sol2(): Int = displays.sumOf { display ->
+    lateinit var `1`: Set<Char>
+    lateinit var `7`: Set<Char>
+    lateinit var `4`: Set<Char>
 
-        for (pattern in display.patterns) when (pattern.size) {
-            3 -> `7` = pattern
-            4 -> `4` = pattern
-            2 -> `1` = pattern
-        }
-
-        val `length=5` = display.patterns.filter { it.size == 5 }.map { it.toSet() }
-        val `2` = `length=5`.first { (it + `4`).size == 7 }
-
-        val top = `7` - `1`
-        val middle = `2`.intersect(`4` - `1`)
-        val down = `length=5`.first { ((it - `4`) - top).size == 1 }.also { (it - `4`) - top }
-        val tLeft = (`4` - `1`) - `2`
-        val tRight = `1`.intersect(`2`)
-        val dLeft = ((`2` - `4`) - top) - down
-        val dRight = `1` - tRight
-
-        val zero = regexFor(down, tRight, dLeft, tLeft, dRight, top)
-        val two = regexFor(dLeft, down, top, middle, tRight)
-        val three = regexFor(middle, dRight, down, tRight, top)
-        val five = regexFor(down, top, middle, dRight, tLeft)
-        val six = regexFor(down, top, middle, dRight, tLeft, dLeft)
-        val nine = regexFor(down, tLeft, middle, tRight, dRight, top)
-
-        display.output.joinToString("") { with(it) {
-            when {
-                length == 2 -> "1"
-                length == 4 -> "4"
-                length == 3 -> "7"
-                length == 7 -> "8"
-                zero.matches(it) -> "0"
-                two.matches(it) -> "2"
-                three.matches(it) && length == 5 -> "3"
-                five.matches(it) && length == 5 -> "5"
-                six.matches(it) -> "6"
-                nine.matches(it) -> "9"
-                else -> throw RuntimeException(it)
-            }}
-        }.toInt()
+    for (pattern in display.patterns) when (pattern.size) {
+        3 -> `7` = pattern
+        4 -> `4` = pattern
+        2 -> `1` = pattern
     }
 
-    println("Sol1: " + displays.flatMap { it.output }.count { it.length in setOf(2, 3, 4, 7) })
-    println("Sol2: $sol2")
+    val `length=5` = display.patterns.filter { it.size == 5 }.map { it.toSet() }
+    val `2` = `length=5`.first { (it + `4`).size == 7 }
+
+    val top = `7` - `1`
+    val mid = `2`.intersect(`4` - `1`)
+    val down = `length=5`.first { ((it - `4`) - top).size == 1 }.also { (it - `4`) - top }
+    val tL = (`4` - `1`) - `2`
+    val tR = `1`.intersect(`2`)
+    val dL = ((`2` - `4`) - top) - down
+    val dR = `1` - tR
+
+    display.output.joinToString("") { with(it) {
+        when {
+            length == 2 -> "1"
+            length == 4 -> "4"
+            length == 3 -> "7"
+            length == 7 -> "8"
+            regexFor(down, tR, dL, tL, dR, top).matches(it) -> "0"
+            regexFor(dL, down, top, mid, tR).matches(it) -> "2"
+            regexFor(mid, dR, down, tR, top).matches(it) && length == 5 -> "3"
+            regexFor(down, top, mid, dR, tL).matches(it) && length == 5 -> "5"
+            regexFor(down, top, mid, dR, tL, dL).matches(it) -> "6"
+            regexFor(down, tL, mid, tR, dR, top).matches(it) -> "9"
+            else -> throw RuntimeException(it)
+        }}
+    }.toInt()
 }
+
+println("Sol1: " + displays.flatMap { it.output }.count { it.length in setOf(2, 3, 4, 7) })
+println("Sol2: " + sol2())
