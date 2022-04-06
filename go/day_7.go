beb5cf84f593288fac47d6bef0bdf22d3015bf73
Pedro Costa <pedro.costa2@sky.uk>
2022-04-06 18:57:03 +0100

    adds day7

---
+++ go/day_7.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	. "github.com/pscosta/go-strm/strm"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var crabs []int
+
+func readInput() {
+	file, _ := os.Open("input7.txt")
+	scanner := bufio.NewScanner(file)
+	defer file.Close()
+
+	// parse lines
+	for scanner.Scan() {
+		crabs = RangeFrom(
+			Map(From(strings.Split(scanner.Text(), ",")),
+				func(f string) int { fi, _ := strconv.Atoi(f); return fi },
+			).ToSlice()).
+			Sorted().
+			ToSlice()
+	}
+}
+
+func main() {
+	readInput()
+	var avg = Sum(From(crabs)) / len(crabs)
+	var middle = crabs[len(crabs)/2]
+
+	fmt.Printf("Sol1: %v\n", From(crabs).SumBy(
+		func(it int) int { return int(math.Abs(float64(middle - it))) }))
+
+	fmt.Printf("Sol2: %v\n", From(crabs).SumBy(
+		func(it int) int {
+			return int((math.Abs(float64(avg-it))*math.Abs(float64(avg-it)) + math.Abs(float64(avg-it))) / 2)
+		}))
+}
